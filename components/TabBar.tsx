8a970002217729ed339468ff28fd02e2efb907dc
AhmedAbdullah125 <ahmednabeeh98@gmail.com>
2026-03-05 02:01:22 +0200

    style: Adjust `TabBar` component layout and spacing by modifying padding, gaps, and button widths.

--- components/TabBar.tsx
+++ components/TabBar.tsx
@@ -22,19 +22,17 @@ const TabBar: React.FC<TabBarProps> = ({ currentTab, onTabChange }) => {
   ];
 
   return (
-    <div className="fixed max-w-[420px] border-2 border-app-card bottom-2 rounded-3xl left-1/2 -translate-x-1/2 w-full bg-app-bg border-t border-app-card pb-safe py-2 px-2 shadow-[0_-4px_12px_rgba(0,0,0,0.03)] z-50">
-      <div className="flex justify-between items-end mx-auto relative w-full">
+    <div className="fixed max-w-[420px] border-2 border-app-card bottom-1 rounded-3xl left-1/2 -translate-x-1/2 w-full bg-app-bg border-t border-app-card shadow-[0_-4px_12px_rgba(0,0,0,0.03)] z-50">
+      <div className="flex justify-between p-1 flex-nowrap items-end mx-auto gap-2 relative w-full">
         {tabs.map((tab) => {
           const isActive = currentTab === tab.id;
           return (
             <button key={tab.id} onClick={() => onTabChange(tab.id)}
-              className={`relative flex flex-col items-center gap-1.5 transition-all duration-300 w-1/5 group outline-none rounded-[20px] py-1
-                ${isActive ? 'bg-app-gold/10 ' : ''}
-              `}
+              className={`relative flex flex-col items-center gap-1 transition-all p-1 duration-300 w-full rounded-[20px] ${isActive ? 'bg-app-gold/10 ' : ''} `}
             >
               <div
                 className={`
-                  relative flex items-center justify-center p-2 rounded-full transition-all duration-300
+                 flex items-center justify-center p-1 transition-all duration-300
                   ${isActive
                     ? 'text-app-gold transform scale-110 '
                     : 'text-gray-400 hover:text-gray-500'
@@ -43,10 +41,7 @@ const TabBar: React.FC<TabBarProps> = ({ currentTab, onTabChange }) => {
               >
                 {tab.icon}
               </div>
-              <span
-                className={`text-[10px] transition-colors pb-1 duration-300 ${isActive ? 'text-app-gold font-semibold' : 'text-gray-400 font-normal'
-                  }`}
-              >
+              <span className={`text-[10px] transition-colors pb-1 duration-300 ${isActive ? 'text-app-gold' : 'text-gray-400'}`}>
                 {tab.label}
               </span>
             </button>
